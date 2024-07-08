3481b548a3f494b18e898ef4bc8103f8e3b7dbd1
PRONOY ROY <91925947+PRONOY0@users.noreply.github.com>
2024-07-08 18:38:14 +0530

    Update StoreContext.jsx url from localhost to backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,8 +4,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  // const url = "https://food-del-backend-yhaw.onrender.com";
-  const url = 'http://localhost:4000';
+  const url = "https://food-del-backend-yhaw.onrender.com";
 
   const [cartItems, setCartItems] = useState([]);
 
