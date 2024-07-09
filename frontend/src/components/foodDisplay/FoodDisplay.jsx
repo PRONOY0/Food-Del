a1598c8cb97a16177475ae690afc9dc5b362bdb6
PRONOY ROY <91925947+PRONOY0@users.noreply.github.com>
2024-07-09 10:25:41 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/foodDisplay/FoodDisplay.jsx
+++ frontend/src/components/foodDisplay/FoodDisplay.jsx
@@ -10,20 +10,18 @@ const FoodDisplay = ({ category }) => {
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => {
-          if (category === "All" || category === item.category) {
-            return (
+        {food_list
+          .filter(item => category === "All" || category === item.category)
+          .map((item) => (
               <FoodItem
-                key={index}
+                key={item._id}
                 id={item._id}
                 price={item.price}
                 name={item.name}
                 description={item.description}
                 image={item.image}
               />
-            );
-          }
-        })}
+        ))}
       </div>
     </div>
   );
